448c1b50306be9f5b526bc71018eb6dbd18adb57
Jens Braeuer <jens.braeuer@commercetools.de>
2013-02-27 14:41:57 +0000

    Expect, that package.sh is run from within the Git checkout.

--- package.sh
+++ package.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
 VERSION=1.1.9
-cd ..
-git clone git://github.com/Yuav/logstash-packaging.git
-wget http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar -O logstash-packaging/usr/share/logstash/logstash.jar
-#wget https://logstash.objects.dreamhost.com/release/logstash-1.1.8-flatjar.jar
-fpm -n logstash -v $VERSION -d default-jre -a all -C logstash-packaging -m "<lesspublic@gmail.com>" --pre-install logstash-packaging/logstash.preinstall --description "Logstash Open Source Log Management" --url 'http://www.logstash.net/' -t deb --config-files etc/logstash/syslog.conf --config-files etc/default/logstash -s dir etc usr var
+
+set -e
+
+wget "http://logstash.objects.dreamhost.com/release/logstash-${VERSION}-monolithic.jar" \
+    -O logstash-packaging/usr/share/logstash/logstash.jar
+fpm1.9.1 \
+    -n logstash \
+    -v "$VERSION" \
+    -d openjdk-7-jre \
+    -a all \
+    -m "<lesspublic@gmail.com>" \
+    --pre-install logstash-packaging/logstash.preinstall \
+    --description "Logstash Open Source Log Management" \
+    --url 'http://www.logstash.net/' \
+    --config-files etc/logstash/syslog.conf \
+    --config-files etc/default/logstash \
+    -t deb \
+    -s dir etc usr var
